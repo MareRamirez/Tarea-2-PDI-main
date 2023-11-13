7e6d250d6af7ddcf66acb73db3287a76f74bff2d
MareRamirez <mramirez2019@udec.cl>
2023-11-13 15:13:46 -0300

    arreglo de comentarios

--- ejercicio1.py
+++ ejercicio1.py
@@ -15,17 +15,17 @@ img_superior = original_img[:mitad_vertical, :] # Se obtiene la imagen superior
 img_espejo_superior = cv2.flip(img_superior, 0)  # 0 para espejo vertical
 resultado = np.vstack((img_superior, img_espejo_superior)) # Se apilan las imagenes
 
-# Mostrar la img con el efecto de espejo en una sola figura
-plt.figure() # Se crea una figura
-plt.subplot(1, 2, 1) # Se crea una subfigura
-plt.title('img original') # Se agrega un titulo a la subfigura
+# Mostrar la imagen con el efecto de espejo en una sola figura
+plt.figure() 
+plt.subplot(1, 2, 1) 
+plt.title('img original') 
 plt.imshow(cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)) # Se muestra la img original
 
-plt.subplot(1, 2, 2) # Se crea una subfigura
-plt.title('img numeros original y espejo') # Se agrega un titulo a la subfigura
+plt.subplot(1, 2, 2) 
+plt.title('img numeros original y espejo') 
 plt.imshow(cv2.cvtColor(resultado, cv2.COLOR_BGR2RGB)) # Se muestra la img con efecto de espejo
 
-plt.show() # Se muestra la figura
+plt.show() 
 
 
 #Parte 1.B 
@@ -35,7 +35,7 @@ x2, y2 = 750, 750 # Esquina inferior derecha
 region_to_smooth = original_img[y1:y2, x1:x2] 
 
 # Definir el tamaño de la ventana del filtro de media
-window_size = (7, 7)  # Ajusta el tamaño según tu preferencia
+window_size = (7, 7)  # Ajusta del tamaño 
 
 # Crear el filtro de media
 filtro_media = np.ones(window_size, dtype=np.float32) / (window_size[0] * window_size[1])
@@ -47,7 +47,6 @@ region_suavizada = cv2.filter2D(region_to_smooth, -1, filtro_media)
 imagen_resultante = np.copy(original_img)
 imagen_resultante[y1:y2, x1:x2] = region_suavizada
 
-# Mostrar la imagen original y la imagen con la región suavizada en una sola figura
 plt.figure()
 plt.subplot(1, 2, 1)
 plt.imshow(cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB))
@@ -107,7 +106,7 @@ plt.show()
 #Parte 1.D
 original_img = cv2.imread('numbers.jpg', cv2.IMREAD_GRAYSCALE)
 sob = filters.sobel(original_img) # Aplicar el filtro de Sobel
-umbral = sob.max() * 0.5 # Calcular el umbral para binarizar la imagen
+umbral = sob.max() * 0.5 # Umbral para binarizar la imagen
 sob_b = (sob <= umbral).astype(np.uint8) * 255 # Binarizar la imagen
 
 plt.figure(figsize=(15, 15))
