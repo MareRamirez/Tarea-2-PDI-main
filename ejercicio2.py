e3111e20e2eb806a1468a9a08564f3b44ee0f6c8
Ana Vargas <avargas2020@inf.udec.cl>
2023-11-13 01:06:36 -0300

    Implementacion de ejercicio 2.D

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,7 +2,6 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
 from skimage import filters
 
 # Parte 2.A
@@ -49,7 +48,7 @@ plt.axis('off')
 plt.show()
 
 # Parte 2.B
-# Aplicar la m ́ascara obtenida a la imagen original y desplegar la imagen original junto a la
+# Aplicar la mascara obtenida a la imagen original y desplegar la imagen original junto a la
 # imagen obtenida, ambas en escala de grises
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Se convierte la imagen a escala de grises
@@ -66,33 +65,35 @@ plt.axis('off')
 
 plt.subplot(1, 2, 2)
 plt.imshow(img_gray, cmap='gray')
-plt.title('Imagen con mascara')
+plt.title('Imagen con máscara')
 plt.axis('off')
 
 plt.show()
 
-
 # Parte 2.C 
-# Aplicar los filtros de roberts, sobel y Prewitt a la imagen obtenida en el punto anterior 
+# Aplicar los filtros de Roberts, Sobel y Prewitt a la imagen obtenida en el punto anterior 
+
+# Se aplica el filtro de Roberts, Sobel y Prewitt a la imagen obtenida en el punto anterior
 roberts = filters.roberts(img_gray)
 prewitt = filters.prewitt(img_gray)
 sobel = filters.sobel(img_gray)
 
+# Se muestran las imagenes con los filtros aplicados
 plt.figure(figsize=(15, 5))
 
 plt.subplot(1, 3, 1)
 plt.imshow(np.abs(roberts), cmap='gray')
-plt.title('robertserts con Máscara')
+plt.title('Roberts con máscara')
 plt.axis('off')
 
 plt.subplot(1, 3, 2)
 plt.imshow(np.abs(sobel), cmap='gray')
-plt.title('sobelel con Máscara')
+plt.title('Sobel con máscara')
 plt.axis('off')
 
 plt.subplot(1, 3, 3)
 plt.imshow(np.abs(prewitt), cmap='gray')
-plt.title('Prewitt con Máscara')
+plt.title('Prewitt con máscara')
 plt.axis('off')
 
 plt.show()
@@ -103,58 +104,48 @@ plt.show()
 # verdes. Escoja la imagen de bordes obtenida en 2.c) que considere mejor. Recuerde que para
 # realizar esto, es necesario trabajar cada canal por separado. 
 
-# Asumimos que 'img' es la imagen original y 'prewitt' es la imagen de bordes obtenida con el filtro de Prewitt.
+# Convierte la imagen original a tonos de verde
+img_verde = img.copy()
+img_verde[:, :, 0] = 0  
+img_verde[:, :, 2] = 0  
+
+# Convertir la imagen a escala de grises
+img_gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Mezclar la imagen en escala de grises con el canal verde
+img_verde_mezclado = cv2.addWeighted(img_verde, 0.5, cv2.merge([img_gray_scale, img_gray_scale, img_gray_scale]), 0.5, 0)
+
+roberts = filters.roberts(img_gray)
+
+# Se normalizar y umbralizar la imagen de bordes Roberts
+roberts_normalizado = cv2.normalize(roberts, None, 0, 255, cv2.NORM_MINMAX)
+_, mask_bordes = cv2.threshold(roberts_normalizado, 25, 255, cv2.THRESH_BINARY)  
 
 # Convertir la imagen original a tonos de verde
-# Esto se hace poniendo los canales rojo y azul en cero y dejando el canal verde como está
-img_verde = np.zeros_like(img)
-img_verde[:, :, 1] = img[:, :, 1]  # Mantener solo el canal verde
+img_verde_mezclado = cv2.addWeighted(img_verde, 0.5, cv2.merge([img_gray_scale, img_gray_scale, img_gray_scale]), 0.5, 0)
 
-# Ahora, necesitamos resaltar los bordes en amarillo
-# Esto se logra asegurándose de que, donde haya bordes, los canales rojo y verde sean altos y el azul sea bajo o cero
-# Primero, normalizamos la imagen de bordes para que su rango sea entre 0 y 255
-prewitt_normalizado = (prewitt / prewitt.max()) * 255
-prewitt_normalizado = prewitt_normalizado.astype(np.uint8)
+# Aplicar bordes amarillos en la imagen 
+img_rgb = img_verde_mezclado.copy()
+img_rgb[mask_bordes > 0] = [0, 165, 255]  
 
-# Crear una máscara donde existan bordes
-mask_bordes = prewitt_normalizado > 0
-
-# Donde hay bordes, establecer el canal rojo y verde al valor del borde (para hacer amarillo), y dejar el azul en cero
-img_rgb = img_verde.copy()
-img_rgb[mask_bordes] = [255, 255, 0]  # Amarillo (rojo y
-                                        # verde altos, azul bajo)
-
-# Mostrar la imagen RGB
+# Mostrar la imagen 
 plt.figure(figsize=(10, 5))
-plt.imshow(img_rgb)
-plt.title('Imagen RGB')
+plt.imshow(cv2.cvtColor(img_rgb, cv2.COLOR_BGR2RGB))
+plt.title('Imagen con bordes de Roberts')
 plt.axis('off')
 plt.show()
-                                      
 
+# Parte 2.E
+# Dada la imagen original cargada en el paso a) (“eye.jpg”) obtenga una imagen del ojo sin los
+# vasos sanguineos de este. Para lograr este resultado, se puede apoyar en la mascara de las venas
+# y arterias obtenida en el paso a). Luego, a la imagen resultante, aplicar un filtro de suavizado
+# para desenfocarlo y despliegue los resultados. Utilizar como fondo la imagen desenfocada que
+# obtuvo en el paso anterior. Finalmente, debe lograr que las venas y las arterias se resalten
+# con color verde y que el fondo difuminado sea de color azul. Puede realizar esto asignando
+# la mascara y el fondo por separado a los canales de una imagen RGB utilizando el comando
+# np.dstack(R,G,B), para unificar distintos canales en una sola imagen.
 
+# Crear la máscara invertida
+# mascara_invertida = 255 - mascara_completa
 
-
-
-
-
-
-# # Se crea una imagen RGB con las dimensiones de la imagen original
-# img_rgb = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-
-# # Se asigna el canal verde de la imagen original al canal verde de la imagen RGB
-# img_rgb[:, :, 1] = img[:, :, 1]
-
-# # Se asigna el canal rojo de la imagen de bordes al canal rojo de la imagen RGB
-# img_rgb[:, :, 0] = np.abs(prewitt)
-
-# # Se asigna el canal azul de la imagen de bordes al canal azul de la imagen RGB
-# img_rgb[:, :, 2] = np.abs(prewitt)
-
-# # Se muestra la imagen RGB
-# plt.figure(figsize=(10, 5))
-# plt.imshow(img_rgb)
-# plt.title('Imagen RGB')
-# plt.axis('off')
-# plt.show()
 
